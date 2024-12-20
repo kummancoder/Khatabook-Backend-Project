576943f8e53242b014aa86e6568068d87433a145
kummancoder <kummandhakad@gmail.com>
2024-12-20 23:49:43 +0530

    Server Created

---
+++ App.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Set up static folder for serving CSS
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Tailwind with EJS' });
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./views/**/*.ejs"], // Add this line to include all EJS files in your views folder
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
